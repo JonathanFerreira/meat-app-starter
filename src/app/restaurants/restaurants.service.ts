0f80b30e3cb28ef7168df62f57fcb1874da8cb42
JonathanFerreira <jonathan.vferreira1@gmail.com>
2017-11-06 21:08:38 -0200

    feat(ErrorHandler): Adição de classe para tratar erros

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,8 +2,10 @@ import {Injectable} from '@angular/core'
 import {Http} from '@angular/http'
 import {Observable} from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 import {Restaurant} from './restaurant/restaurant.model'
 import {MEAT_API} from '../app.api'
+import {ErrorHandler} from '../app.error-handler'
 
 @Injectable()
 
@@ -11,6 +13,8 @@ export class RestaurantsService {
   constructor(private http: Http){}
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+    return this.http.get(`${MEAT_API}/restaurants`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError)
   }
 }
